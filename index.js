14ede277fa6f07fa6c24e19ab2fc68edd34514da
TheSerch07 <jejog50@gmail.com>
2023-03-21 10:51:35 -0500

    server init

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
-app.use()
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
