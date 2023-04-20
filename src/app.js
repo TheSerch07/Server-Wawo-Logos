f7d51d79651ba834eda107f1b9e8c4620f4e09b6
TheSerch07 <jejog50@gmail.com>
2023-04-20 14:51:28 -0500

    Correos

--- src/app.js
+++ src/app.js
@@ -20,12 +20,24 @@ server.get("/", (req, res) => {
 server.post("/sendEmail", async (req, res) => {
     
     try {
-        const {nombre} = req.body
+        const {nombre, cuentanosMas, palabras, eslogan, estilos, correo} = req.body
         await transporter.sendMail({
             from: `"Hola, ${nombre} está a la espera de su nuevo logo. 👀" <sergio@whateverworks.design>"`,
             to: ["jejog50@gmail.com", "sergio@whateverworks.design", "mauricio.gaitan@whateverworks.design", "laura.gutierrez@whateverworks.design", "angie.castillo@whateverworks.design", "hello@whateverworks.design"],
             subject: "¡Hola, equipo! ¡Tenemos un nuevo cliente a la espera de su nuevo logo! 😋",
-            html: `<div>Mira el <a href="https://docs.google.com/spreadsheets/d/14sWd8VUsQKuvGsnEAyCSSJTsWBYAXnrQyx5od5jPqRA/edit#gid=0">Sheets</a> para comenzar a crear el logo. 👨‍🚀</div>`
+            html: `<div>
+                    <h1>Los datos del cliente</h1>
+                    <ul>
+                        <li>Acerca de la empresa: ${cuentanosMas}</li>
+                        <li>Palabras clave: ${palabras}</li>
+                        <li>Eslogan: ${eslogan}</li>
+                        <li>Estilo de logo: ${estilos}</li>
+                        <li>Correo: ${correo}</li>
+                    </ul>
+                <div>
+                Mira el <a href="https://docs.google.com/spreadsheets/d/14sWd8VUsQKuvGsnEAyCSSJTsWBYAXnrQyx5od5jPqRA/edit#gid=0">Sheets</a> para comenzar a crear el logo. 👨‍🚀
+                </div>
+            </div>`
         })
         res.status(200).json("Email enviado!")
     } catch (err) {
